24105d45a28337c95682046631d08056919a524b
Eamonn Hughes <efohughes@gmail.com>
2023-12-03 11:11:31 -0500

    1a done

--- src/main/scala/Ex1a.scala
+++ src/main/scala/Ex1a.scala
@@ -1,5 +1,16 @@
 package org.eamonnh.aoc23
 
+import scala.io.Source
+
 object Ex1a extends App {
-println("hello world")
+
+  val fileName = "input/Ex1aInput.txt"
+  var ints: List[Int] = List.empty
+for (line <- Source.fromFile(fileName).getLines) {
+  val regex = "\\d+".r
+  val numberized = regex.findAllIn(line).flatMap(_.map(_.toString.toInt)).toList
+  val num = s"${numberized.head}${numberized.reverse.head}"
+  ints = num.toInt :: ints
+}
+  println(ints.sum)
 }
